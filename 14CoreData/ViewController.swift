09e1b4b114ea5a9ca5e660cfec74e3c96017f8ae
Sergey Lobanovskiy <sergey.lobanovsky@gmail.com>
2020-10-24 14:27:42 +0300

    Initial Commit

---
+++ 14CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14CoreData
+//
+//  Created by Sergey Lobanovskiy on 24.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
