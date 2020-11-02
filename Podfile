6229284ccd1b026df696f976db50b6591a2efb16
Sergey Lobanovskiy <sergey.lobanovsky@gmail.com>
2020-11-02 18:27:23 +0300

    fix

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '14.1'
+use_frameworks!
+
+target '14CoreData' do
+
+pod 'RealmSwift'
+pod 'Alamofire'
+
+end
---
+++ Pods/Target Support Files/Pods-14CoreData/Pods-14CoreData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_14CoreData : NSObject
+@end
+@implementation PodsDummy_Pods_14CoreData
+@end
